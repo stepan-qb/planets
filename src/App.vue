f9473611f11e5d1759ace759438c1b30ddc1fe97
stepan.k <stepan.k@qb-online.ru>
2019-01-30 16:07:20 +0300

    persons list && person detail in modal

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div :class="$options.name">
-    <div class="list">
-      <div
-        v-for="item in list"
-        :key="item.id"
-        class="item"
-        @click="$modal.show('planet', item)"
-      >
-        {{ item.name }}
+    <transition v-if="list && list.length" name="fade" appear>
+      <div class="list">
+        <div
+          v-for="item in list"
+          :key="item.id"
+          class="item"
+          @click="showPerson(item)"
+        >
+          {{ item.name }}
+        </div>
       </div>
-    </div>
+    </transition>
+
+    <div v-else class="spinner"></div>
 
     <planet-modal />
   </div>
@@ -26,6 +30,7 @@ export default {
   },
   data: () => ({
     list: null,
+    current: null,
   }),
   created() {
     axios('https://swapi.co/api/people/', {
@@ -37,22 +42,171 @@ export default {
       console.log(e);
     });
   },
+  methods: {
+    showPerson(person) {
+      if (!this.current || this.current.name !== person.name) {
+        this.current = person;
+        this.current.shipsList = [];
+        this.current.filmsList = [];
+        let shipsActions = [];
+        let filmsActions = [];
+        person.starships.forEach(url => {
+          shipsActions.push(this.getShip(url));
+        });
+
+        person.films.forEach(url => {
+          filmsActions.push(this.getFilm(url));
+        });
+        Promise.all([...shipsActions, ...filmsActions])
+          .then(() => {
+            this.$modal.show('person', this.current);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      } else {
+        this.$modal.show('person', this.current);
+      }
+    },
+    async getShip(url) {
+      await axios(url, {
+        method: 'GET',
+        mode: 'no-cors',
+      }).then((resp) => {
+        this.current.shipsList.push(resp.data.name);
+      }).catch((e) => {
+        console.log(e);
+      });
+    },
+    async getFilm(url) {
+      await axios(url, {
+        method: 'GET',
+        mode: 'no-cors',
+      }).then((resp) => {
+        this.current.filmsList.push(resp.data.title);
+      }).catch((e) => {
+        console.log(e);
+      });
+    },
+  },
 };
 </script>
 
 <style>
 .app-view {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #4c596f;
+  background-color: #b9cadc;
+  height: 100%;
+}
+
+.list {
+  padding-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .item {
+  padding: 12px;
   cursor: pointer;
   transition: color .3s ease-out;
 }
 
 .item:hover {
-  color: #ccc;
+  color: #668ebb;
+}
+
+.spinner {
+  position: absolute;
+  width: 125px;
+  height: 125px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.spinner:before, .spinner:after {
+  content: "";
+  display: block;
+  position: absolute;
+  border-width: 4px;
+  border-style: solid;
+  border-radius: 50%;
+}
+
+.spinner:before {
+  width: 117px;
+  height: 117px;
+  border-bottom-color: #3d3339;
+  border-right-color: #3d3339;
+  border-top-color: rgba(33, 33, 33, 0);
+  border-left-color: rgba(33, 33, 33, 0);
+  top: 0;
+  left: 0;
+  -webkit-animation: rotate-animation 1s linear 0s infinite;
+  animation: rotate-animation 1s linear 0s infinite;
+}
+.spinner:after {
+  width: 81.9px;
+  height: 81.9px;
+  border-bottom-color: #ad846e;
+  border-right-color: #ad846e;
+  border-top-color: rgba(33, 33, 33, 0);
+  border-left-color: rgba(33, 33, 33, 0);
+  top: 17.55px;
+  left: 17.55px;
+  -webkit-animation: anti-rotate-animation 0.85s linear 0s infinite;
+  animation: anti-rotate-animation 0.85s linear 0s infinite;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+@-webkit-keyframes rotate-animation {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes rotate-animation {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes anti-rotate-animation {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(-360deg);
+    transform: rotate(-360deg);
+  }
+}
+@keyframes anti-rotate-animation {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(-360deg);
+    transform: rotate(-360deg);
+  }
 }
 </style>
--- src/components/modal/DetailView.vue
+++ src/components/modal/DetailView.vue
@@ -3,16 +3,50 @@
     <modal
       adaptive
       height="360"
-      name="planet"
+      name="person"
       @before-open="beforeOpen"
     >
       <header>
-        <h4 class="title">{{ name }}</h4>
-        <span name="close" class="close" @click="$modal.hide('planet')">+</span>
+        <h4 class="title">{{ person.name }}</h4>
+        <span name="close" class="close" @click="$modal.hide('person')">+</span>
       </header>
 
       <div class="content">
-        <p class="description">{{ description }}</p>
+        <div class="stats">
+          <div class="stat">
+            <span class="label">Возраст: </span>
+            <span class="value">{{ person.birth_year }}</span>
+          </div>
+
+          <div class="stat">
+            <span class="label">Пол: </span>
+            <span class="value">{{ person.gender }}</span>
+          </div>
+
+          <div class="stat">
+            <span class="label">Рост: </span>
+            <span class="value">{{ person.height }}</span>
+          </div>
+
+          <div class="stat">
+            <span class="label">Вес: </span>
+            <span class="value">{{ person.mass }}</span>
+          </div>
+
+          <div v-if="person.shipsList.length" class="stat">
+            <span class="label">Корабли: </span>
+            <ul class="value">
+              <li v-for="ship in person.shipsList" :key="ship" class="value-item"><span>{{ ship }}</span></li>
+            </ul>
+          </div>
+
+          <div v-if="person.filmsList.length" class="stat">
+            <span class="label">Фильмы: </span>
+            <ul class="value">
+              <li v-for="film in person.filmsList" :key="film" class="value-item"><span>{{ film }}</span></li>
+            </ul>
+          </div>
+        </div>
       </div>
 
     </modal>
@@ -20,16 +54,27 @@
 </template>
 
 <script>
+const createDefaultData = () => ({
+  name: null,
+  birth_year: null,
+  gender: null,
+  mass: null,
+  height: null,
+  shipsList: [],
+  filmsList: [],
+});
+
 export default {
   name: 'modal-detail-view',
   data: () => ({
-    name: '',
-    description: '',
+    person:  createDefaultData(),
   }),
   methods: {
     beforeOpen({ params }) {
-      this.name = params.name;
-      this.description = params.description;
+      this.person = {
+        ...params,
+        gender: params.gender === 'male' ? 'Мужчина' : 'Женщина',
+      };
     },
   },
 };
@@ -48,6 +93,7 @@ header {
 }
 
 .title {
+  color: #120f12;
   margin: 0;
   font-weight: 600;
 }
@@ -59,10 +105,28 @@ header {
   overflow-y: auto;
 }
 
+.label {
+  font-weight: 600;
+  color: #4c596f;
+}
+
+.value {
+  color: #120f12;
+}
+
+.value-item {
+  padding-top: 6px;
+}
+
+.stat + .stat {
+  margin-top: 12px;
+}
+
 .close {
   width: 24px;
   height: 24px;
   font-size: 24px;
+  color: #120f12;
   flex-shrink: 0;
   transform: rotate(45deg);
   cursor: pointer;
